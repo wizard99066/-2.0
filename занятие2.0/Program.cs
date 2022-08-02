3d37e6ad1a3fd135480c12b4a41f2d881e7b585a
vlas_m <vlas_m@WS-665>
2022-08-02 09:42:54 +0300

    Добавьте файлы проекта.

---
+++ занятие2.0/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 5 | n == 10)
+{
+    Console.WriteLine("введенное число равно 5 или 10");
+}
+else
+
+    Console.WriteLine("неизвестное число");
+
